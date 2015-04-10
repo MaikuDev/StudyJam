18d4c0e1a10a5c75f42022c00ccacd6ed51e7645
mike1991 <black.mmms@gmail.com>
2015-04-10 04:01:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mauku/studyjam/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mauku.studyjam;
+
+import android.app.Activity;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+    private CustomPagerAdapter mCustomPagerAdapter;
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mCustomPagerAdapter = new CustomPagerAdapter(this);
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mCustomPagerAdapter);
+    }
+}
